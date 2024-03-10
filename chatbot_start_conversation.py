64d26e8e1668960529d9870dd26430d240d32ef9
sonutelkar <srivatsa.telkar@protonmail.com>
2024-03-10 09:47:05 +0000

    small edit

--- chatbot_start_conversation.py
+++ chatbot_start_conversation.py
@@ -20,10 +20,6 @@ PPLX_API_KEY = os.getenv('PLX_API_KEY')
 load_dotenv(find_dotenv())
 os.environ['TOKENIZERS_PARALLELISM'] = '(true | false)'
 
-n_gpu_layers = 1  # Metal set to 1 is enough.
-# Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
-n_batch = 512
-callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 class Chatbot:
     def __init__(self):
         self.__initialise_LLM()
@@ -31,15 +27,6 @@ class Chatbot:
 
 
     def __initialise_prompt_template(self):
-        # create the prompt
-        # self.__prompt_template: str = """/
-        # You are a medical assissant that will be helping patients to enter the correct information required
-        # for a proper diagosis of their symptoms. The patients will have to enter their name, gender and year of birth and a symptoms of their illness.
-        # Check if the user has entered their name, gender and year of birth and a symptoms of their illness. If so then repeat their name, gender and year of birth and symptons. If not then kindly tell which infomation is missing
-        #
-        # patient:{info}
-        # """
-        # self.__prompt = PromptTemplate.from_template(template=self.__prompt_template)
         self.__system = """You are a medical assissant that will be helping patients to enter the correct information required
         for a proper diagosis of their symptoms. The patients will have to enter their name, gender and year of birth and a symptoms of their illness.
         Check if the user has entered their name, gender and year of birth and a symptoms of their illness. If so then repeat their name, gender and year of birth and symptons. 
